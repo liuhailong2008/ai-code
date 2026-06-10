36fa9e1f69fbeb9deeea4f8179003202e67eceab
Liu Hailong <liuhailong2008@foxmail.com>
2026-06-10 11:49:59 +0800

    Remove front-demo, doc directories and AGENTS.md

--- front-demo/code/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './styles/themes.css'
-import './styles/base.css'
-
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
